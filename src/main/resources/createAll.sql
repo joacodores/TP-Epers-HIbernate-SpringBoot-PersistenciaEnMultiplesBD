336e9833dad4b13d51dbf4ddbe2b2f6478d67272
salvanescki <salvanescki.unq@gmail.com>
2025-08-28 00:03:35 -0300

    fix: set field nombre to not unique

--- src/main/resources/createAll.sql
+++ src/main/resources/createAll.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS espiritu (
     id SERIAL8 PRIMARY KEY,
     tipo VARCHAR(255),
     niveldeconexion int NOT NULL,
-    nombre VARCHAR(255) UNIQUE
+    nombre VARCHAR(255)
 );
